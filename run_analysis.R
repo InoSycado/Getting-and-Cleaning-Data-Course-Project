c997c17994599c78016cedab230ffa0a108f6ada
Inooooo <rizalino35@gmail.com>
2025-06-20 10:17:42 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, destfile = "dataset.zip")
+unzip("dataset.zip")
+
+# Load required libraries
+library(dplyr)
+
+# 1. Read data
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("index", "feature"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+# Training data
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$feature)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+
+# Test data
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$feature)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+
+# 2. Merge the datasets
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+subject_data <- rbind(subject_train, subject_test)
+
+merged_data <- cbind(subject_data, y_data, x_data)
+
+# 3. Extract only mean and std measurements
+tidy_data <- merged_data %>%
+  select(subject, activity, contains("mean"), contains("std"))
+
+# 4. Use descriptive activity names
+tidy_data$activity <- activities[tidy_data$activity, 2]
+
+# 5. Clean variable names
+names(tidy_data) <- gsub("^t", "Time", names(tidy_data))
+names(tidy_data) <- gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data) <- gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data) <- gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data) <- gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data) <- gsub("BodyBody", "Body", names(tidy_data))
+
+# 6. Create final tidy dataset with the average of each variable for each activity and each subject
+final_data <- tidy_data %>%
+  group_by(subject, activity) %>%
+  summarise_all(mean)
+
+# 7. Write output
+write.table(final_data, "tidy_data.txt", row.name = FALSE)
